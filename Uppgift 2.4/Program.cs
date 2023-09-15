30eb4fef620998f896702bbb3055cea0105655e3
molo06002 <molo06002@E-PW0234KQ>
2023-09-15 10:48:40 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Skriv in lönen för medarbetare 1");
+            string x = Console.ReadLine();
+            int y = Convert.ToInt32(x);
+            Console.WriteLine("Skriv in lönen för medarbetare 2");
+            string k = Console.ReadLine();
+            int o = Convert.ToInt32(k);
+            Console.WriteLine("Skriv in lönen för medarbetare 3");
+            string p = Console.ReadLine();
+            int u = Convert.ToInt32(p);
+            int t = y + o + u;
+            int R = t / 3;
+            Console.WriteLine("Medellönen för alla är: " + R);
+        }
+    }
+}
